1ba1f628e321a51de19df79ce28084099b29b04d
drcat <agytyutytt@gamil.com>
2020-05-07 02:21:57 +0800

    云端部署小修改

--- config/config.php
+++ config/config.php
@@ -2,11 +2,14 @@
 define("nLine","<br>");
 define("ROOTDIR",$_SERVER['DOCUMENT_ROOT']);
 
+$web=$_SERVER["HTTP_ORIGIN"];
+
 include_once("utils/requestUtils.php");
 include_once("utils/authorizationUtils.php");
 include_once("utils/commonUtils.php");
 
-header('Access-Control-Allow-Origin: http://localhost:8081');
+if(!$web) header('Access-Control-Allow-Origin: http://localhost:8081');
+else header('Access-Control-Allow-Origin:'.$web);
 //header('Access-Control-Allow-Methods:OPTIONS, GET, POST'); // 允许option，get，post请求
 header('Access-Control-Allow-Methods: *'); // 允许option，get，post请求
 //header('Access-Control-Allow-Headers:x-requested-with'); // 允许x-requested-with请求头
--- index_test.php
+++ index_test.php
@@ -1,13 +1,17 @@
 <?php
 
-include ("./utils/commandUtils.php");
-include_once("./app/service/system/systemService.php");
-include_once("./app/service/login/loginService.class.php");
-
-//$loginService=new loginService();
+//include ("./utils/commandUtils.php");
+//include_once("./app/service/system/systemService.php");
+//include_once("./app/service/login/loginService.class.php");
 //
-//print_r($loginService->checkPassword("drcat","12374111"));
+////$loginService=new loginService();
+////
+////print_r($loginService->checkPassword("drcat","12374111"));
+//
+//if(stripos("demo",".")==null){
+//    echo "false";
+//}
 
-if(stripos("demo",".")==null){
-    echo "false";
-}
+$time=$_SERVER['REQUEST_TIME'];
+$arr=array("time"=>$time);
+echo md5(json_encode($arr));
